df9dd172a839d8f031109e7d76f4381d9734b769
VSHEV92 <59792635+VSHEV92@users.noreply.github.com>
2025-08-05 23:39:45 +0300

    add comments

--- examples/led_matrix_max7219/components/max7219/include/driver/max7219.h
+++ examples/led_matrix_max7219/components/max7219/include/driver/max7219.h
@@ -28,30 +28,103 @@
 
 extern uint8_t MAX7219_DATA_REGS [];
 
+
+/**
+ * MAX7219 Configuration structure
+ */
 typedef struct {
-    uint8_t mosi;
-    uint8_t sclk;
-    uint8_t cs;
-    uint8_t decode_mask;
-    uint8_t intensity;
-    uint8_t scan_limit;
+    uint8_t mosi;           // MOSI GPIO Number
+    uint8_t sclk;           // SCLK GPIO Number
+    uint8_t cs;             // CS GPIO Number
+    uint8_t decode_mask;    // Initial decode mask
+    uint8_t intensity;      // Initial intensity
+    uint8_t scan_limit;     // Initial Scan-Limit
 } max7219_config_t;
 
 
+/**
+ * @brief Initialize max7219 device
+ *
+ * @param cfg MAX7219 configuration structure
+ * @return spi_device_handler
+ */
 spi_device_handle_t max7219_init(max7219_config_t* cfg);
 
+
+/**
+ * @brief Send data max7219 device
+ *
+ * @param spi_dev SPI device handler
+ * @param addr register address
+ * @param data register value
+ * @return void
+ */
 void max7219_send(spi_device_handle_t spi_dev, uint8_t addr, uint8_t data);
 
+
+/**
+ * @brief Enable shutdown
+ *
+ * @param spi_dev SPI device handler
+ * @return void
+ */
 void max7219_shutdown_enable(spi_device_handle_t spi_dev);
+
+
+/**
+ * @brief Disable shutdown
+ *
+ * @param spi_dev SPI device handler
+ * @return void
+ */
 void max7219_shutdown_disable(spi_device_handle_t spi_dev);
 
+
+/**
+ * @brief Enable display test mode
+ *
+ * @param spi_dev SPI device handler
+ * @return void
+ */
 void max7219_display_test_enable(spi_device_handle_t spi_dev);
+
+
+/**
+ * @brief Disable display test mode
+ *
+ * @param spi_dev SPI device handler
+ * @return void
+ */
 void max7219_display_test_disable(spi_device_handle_t spi_dev);
 
+
+/**
+ * @brief Set scan limit
+ *
+ * @param spi_dev SPI device handler
+ * @param limit Scan-Limit value
+ * @return void
+ */
 void max7219_scan_limit_set(spi_device_handle_t spi_dev, uint8_t limit);
 
+
+/**
+ * @brief Set decode mask
+ *
+ * @param spi_dev SPI device handler
+ * @param mask decode mask value
+ * @return void
+ */
 void max7219_decode_mask_set(spi_device_handle_t spi_dev, uint8_t mask);
 
+
+/**
+ * @brief Set intensity
+ *
+ * @param spi_dev SPI device handler
+ * @param intensity intensity value
+ * @return void
+ */
 void max7219_intensity_set(spi_device_handle_t spi_dev, uint8_t intensity);
 
 #endif
