58456674d90381ad746c581514312f47c45f4154
VSHEV92 <59792635+VSHEV92@users.noreply.github.com>
2025-09-02 00:27:46 +0300

    add example with custom build make files

---
+++ examples/baremetal/custom_build_make/main.c
@@ -0,0 +1,20 @@
+#include <custom_init.h>
+
+// prototypes of esp32c3 rom functions
+int ets_printf(const char *format, ...);
+void ets_delay_us(uint32_t us);
+
+void __attribute__((noreturn)) main() {
+    
+    // run custom soc init
+    custom_init();
+
+    while(1) {
+        ets_printf("Hello from custom Makefile build!\n");
+        
+        // esp_rom think that cpu has default clock frequency 20 MHz
+        // but custom_init configure cpu clock to 160 MHz
+        ets_delay_us(1000000 * (160/20));
+    }
+
+}
