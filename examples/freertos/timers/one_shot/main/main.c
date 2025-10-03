6dea1f12e261eaad63a95cc8a1235aadca5281cb
VSHEV92 <59792635+VSHEV92@users.noreply.github.com>
2025-10-03 22:30:21 +0300

    add freertos one shot timer example

---
+++ examples/freertos/timers/one_shot/main/main.c
@@ -0,0 +1,64 @@
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/timers.h"
+
+#define TIMER_NUMBER 5
+#define TIMER_PERIOD_MS 1000
+
+static const char* TAG = "freertos_example";
+
+void timer_callback( TimerHandle_t xTimer );
+
+void app_main(void) {
+    TimerHandle_t one_shot_timer[TIMER_NUMBER];
+
+    ESP_LOGI(TAG, "Create timers");
+
+    for (int i = 0; i < TIMER_NUMBER; i++) {
+        // create one shot timers
+        one_shot_timer[i] = xTimerCreate (
+            "one_shot",                                     // human readable timer name
+            pdMS_TO_TICKS( TIMER_PERIOD_MS * (i + 1) ),     // timer period in ticks
+            pdFALSE,                                        // disable autoreload
+            ( void * ) i,                                   // set timer ID
+            timer_callback                                  // timer callback function 
+        );
+
+        // check that timer successfully created
+        if (one_shot_timer[i] == NULL) {
+            ESP_LOGE(TAG, "Can't create timer %d!", i);
+            abort();
+        }
+    }
+
+    ESP_LOGI(TAG, "Start timers");
+
+    for (int i = 0; i < TIMER_NUMBER; i++) {
+        // start timer
+        xTimerStart(
+            one_shot_timer[i],  // timer handler
+            portMAX_DELAY       // max delay for waiting until command send to deamon command queue
+        );
+    }
+
+    // wait until all timers are triggered and delete timers
+    vTaskDelay( pdMS_TO_TICKS((TIMER_NUMBER + 1) * TIMER_PERIOD_MS) );
+
+    ESP_LOGI(TAG, "Delete timers");
+
+    for(int i = 0; i < TIMER_NUMBER; i++) {
+        xTimerDelete(
+            one_shot_timer[i],  // timer handler
+            portMAX_DELAY       // max delay for waiting until command send to deamon command queue
+        );
+    }
+}
+
+void timer_callback( TimerHandle_t xTimer ) {
+    // get timer id
+    int timer_id = ( int ) pvTimerGetTimerID( xTimer );
+
+    // print timer message
+    ESP_LOGI(TAG, "Hello from timer %d", timer_id);
+}
