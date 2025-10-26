352607abbdda8354da6e69f673418dac77ffdd37
VSHEV92 <59792635+VSHEV92@users.noreply.github.com>
2025-10-26 21:20:42 +0300

    add app with to tasks

--- examples/freertos_custom_port/src/main.c
+++ examples/freertos_custom_port/src/main.c
@@ -5,6 +5,7 @@
 #define STACK_SIZE 1024
 
 void task_1_function(void* paramenter);
+void task_2_function(void* paramenter);
 
 void __attribute__((noreturn)) main() {
     BaseType_t status;
@@ -25,6 +26,15 @@ void __attribute__((noreturn)) main() {
                     NULL              // task handler
     );
 
+    status = xTaskCreate(
+                    task_2_function,  // called function
+                    "task_2",         // human readable name
+                    STACK_SIZE,       // stack size
+                    NULL,             // task function parameter (void*)
+                    1,                // task priority
+                    NULL              // task handler
+    );
+
     // start scheduler
     vTaskStartScheduler();
     
@@ -35,5 +45,12 @@ void task_1_function(void* paramenter) {
     while(true){
         ets_printf("Hello from task 1!\n");
         vTaskDelay( pdMS_TO_TICKS(500) );
+    }
+}
+
+void task_2_function(void* paramenter) {
+    while(true){
+        ets_printf("Hello from task 2!\n");
+        vTaskDelay( pdMS_TO_TICKS(700) );
     }
 }
