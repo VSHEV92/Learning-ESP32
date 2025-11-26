f9482d454c1de535360a759ecc7f357402d6912b
VSHEV92 <59792635+VSHEV92@users.noreply.github.com>
2025-11-26 22:10:23 +0300

    add bmp180 measure example

--- examples/idf_peripheral_api/i2c/bmp180_measure/main/main.c
+++ examples/idf_peripheral_api/i2c/bmp180_measure/main/main.c
@@ -1,37 +1,34 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 #include "esp_log.h"
-#include "bmp180.h"
-#include "string.h"
+#include "bmp180/bmp180.h"
 
-static const char *TAG = "bmp180_probe";
+static const char *TAG = "bmp180_measure";
 
-#define BMP180_SCL       7
-#define BMP180_SDA       6 
-#define BMP180_ADDR      0x77 
-#define BMP180_FREQ_HZ   10000
-
+#define BMP180_SCL               1
+#define BMP180_SDA               0 
+#define BMP180_MEASURE_PERIOD_MS 1000
 
 void app_main(void) {
-    bmp180_dev_t dev;
-    memset(&dev, 0, sizeof(bmp180_dev_t)); // Zero descriptor
-
-    ESP_ERROR_CHECK(bmp180_init_desc(&dev, 0, BMP180_SDA, BMP180_SCL));
-  //  ESP_ERROR_CHECK(bmp180_init(&dev));
-
-  //  while (1)
-  //  {
-  //      float temp;
-  //      uint32_t pressure;
-
-  //      esp_err_t res = bmp180_measure(&dev, &temp, &pressure, BMP180_MODE_STANDARD);
-  //      if (res != ESP_OK)
-  //          printf("Could not measure: %d\n", res);
-  //      else
-  //          /* float is used in printf(). you need non-default configuration in
-  //           * sdkconfig for ESP8266, which is enabled by default for this
-  //           * example. see sdkconfig.defaults.esp8266
-  //           */
-  //          printf("Temperature: %.2f degrees Celsius; Pressure: %" PRIu32 " Pa\n", temp, pressure);
-
-  //      vTaskDelay(pdMS_TO_TICKS(500));
-  //  }
+    // configure bmp180 device
+    i2c_lowlevel_config bmp180_cfg = {
+        .port = I2C_NUM_0,
+        .pin_sda = BMP180_SDA,
+        .pin_scl = BMP180_SCL,
+    };
+    
+    bmp180_t* bmp180_device = bmp180_init(
+        &bmp180_cfg,                 // bmp180 configuration
+        0,                           // bmp180 device address, 0 - default address 0x77
+        BMP180_MODE_HIGH_RESOLUTION  // OSS - oversampling setting, set to 13.5ms convertion time
+    );
+   
+    // measure temperature and pressure
+    float temperature;
+    uint32_t pressure; 
+    while(true) {
+        bmp180_measure(bmp180_device, &temperature, &pressure);
+        ESP_LOGI(TAG, "Temperature: %.2f C, Pressure: %u Pa", temperature, (unsigned int)pressure);
+        vTaskDelay( pdMS_TO_TICKS(BMP180_MEASURE_PERIOD_MS) );
+    }
 }
