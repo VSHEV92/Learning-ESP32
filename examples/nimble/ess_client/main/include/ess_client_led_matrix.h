3948c87ffd3dee70df25c806bc154d4bf81cd564
VSHEV92 <59792635+VSHEV92@users.noreply.github.com>
2026-02-16 00:10:14 +0300

    finish ess client example example

--- examples/nimble/ess_client/main/include/ess_client_led_matrix.h
+++ examples/nimble/ess_client/main/include/ess_client_led_matrix.h
@@ -1,15 +1,22 @@
 #ifndef ESS_CLIENT_LED_MATRIX_H
 #define ESS_CLIENT_LED_MATRIX_H
 
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/timers.h"
+
 #include "esp_log.h"
 
 #include "nimble/nimble_port.h"
 #include "host/ble_hs.h"
 #include "services/gatt/ble_svc_gatt.h"
 
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "freertos/timers.h"
+#include "max7219.h"
+
+
+#define MAX7219_SCLK 0
+#define MAX7219_MOSI 2
+#define MAX7219_CS 3
 
 void ess_client_led_matrix_init(void);
 void ess_client_led_matrix_start_read(uint16_t conn_handle, uint16_t att_handle);
--- examples/nimble/ess_client/main/src/ess_client_led_matrix.c
+++ examples/nimble/ess_client/main/src/ess_client_led_matrix.c
@@ -1,10 +1,19 @@
 #include "ess_client_led_matrix.h"
+#include "ess_client_led_matrix_digits.h"
 
 static uint16_t ess_server_connection_handle = 0;
 static uint16_t ess_server_chr_temp_celsius_handle = 0;
 
 static TimerHandle_t temp_read_timer;
 static TaskHandle_t led_martix_update_task_handle;
+
+static max7219_t max7219_device =
+{
+    .cascade_size = 1, // number of max7219 devices connected in cacsade
+    .digits = 0,       // number of digits in 7seg or lines in 8x8, 0 - maximum 8 digits 
+    .mirrored = false, // mirror digits in 7seg or flip lines in 8x8 
+    .bcd = false       // enable bcd 
+};
 
 void ess_client_led_matrix_init(void);
 void ess_client_led_matrix_start_read(uint16_t conn_handle, uint16_t att_handle);
@@ -16,6 +25,37 @@ static void led_matrix_update_task(void* param);
 
 
 void ess_client_led_matrix_init(void) {
+    // configure spi bus
+    spi_bus_config_t spi_bus_cfg = {
+        .sclk_io_num = MAX7219_SCLK,
+        .mosi_io_num = MAX7219_MOSI,
+        .miso_io_num = -1,
+        .quadwp_io_num = -1,
+        .quadhd_io_num = -1,
+        .max_transfer_sz = 2,
+        .flags = 0
+    };
+    spi_bus_initialize(SPI2_HOST, &spi_bus_cfg, SPI_DMA_CH_AUTO);
+    
+    max7219_init_desc(
+        &max7219_device,            // pointer to max7219_t structure
+        SPI2_HOST,                  // spi host, SPI2_HOST is only valid host for esp32c3 
+        MAX7219_MAX_CLOCK_SPEED_HZ, // spi clock rate in Hz, set to max clock rate of max7219 - 10 MHz 
+        MAX7219_CS                  // spi CS pin number 
+    );
+
+    // inititalize max7219 device
+    // 1. Disable test
+    // 2. Set max scan limit
+    // 3. Set normal decode mode 
+    // 4. Clear screen
+    // 5. Set minimal brightness
+    max7219_init(&max7219_device);
+
+    // enable shutdown mode
+    max7219_set_shutdown_mode(&max7219_device, true);
+
+
     // create temperature read timer
     temp_read_timer = xTimerCreate (
         "temperature read",                             // human readable timer name
@@ -41,11 +81,13 @@ void ess_client_led_matrix_start_read(uint16_t conn_handle, uint16_t att_handle)
     ess_server_connection_handle = conn_handle;
     ess_server_chr_temp_celsius_handle = att_handle;
     xTimerStart(temp_read_timer, portMAX_DELAY);
+    max7219_set_shutdown_mode(&max7219_device, false);
 }
 
 
 void ess_client_led_matrix_stop_read() {    
     xTimerStop(temp_read_timer, portMAX_DELAY);
+    max7219_set_shutdown_mode(&max7219_device, true);
 }
 
 
@@ -70,6 +112,7 @@ static int temp_read_cb(uint16_t conn_handle, const struct ble_gatt_error *error
 
 static void led_matrix_update_task(void* param) {
     uint32_t temp_value;
+    uint8_t led_matrix_data[8];
 
     while(true){
         xTaskNotifyWait( 
@@ -81,5 +124,18 @@ static void led_matrix_update_task(void* param) {
         
         ESP_LOGI("", "Read ESS Temperature: %dC", temp_value);
     
+
+    // convert temperature to led digits
+    for (int i = 0; i < 4; i++) {
+        led_matrix_data[i] = led_matrix_digit[temp_value % 10][i];
+        led_matrix_data[i+4] = led_matrix_digit[temp_value / 10][i];
+    }
+
+    // display temperature
+    max7219_draw_image_8x8(
+        &max7219_device, // pointer to max7219_t structure
+        0,               // start line of 8x8 
+        led_matrix_data  // data to display
+    );
     }
 }
